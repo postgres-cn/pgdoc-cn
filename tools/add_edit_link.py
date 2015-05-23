8026de8bd3b03570f08f1cd7022728a0e6a78e1b
chenhuajun <chjischj@163.com>
2015-05-23 18:03:41 +0800

    追加在html中添加在线纠错链接的py脚本

---
+++ tools/add_edit_link.py
@@ -0,0 +1,48 @@
+#功能:在html中添加在线纠错链接
+
+import sys
+import os
+import re
+sys.path.append(".") 
+import html2sgml_map
+
+patternInsertPos=re.compile(r'.*<BODY\s+[^>]*>',re.S)
+InsertHTML="""<div style="text-align:right">
+<a style="margin : 0px 0px 0px 10px;" href="https://github.com/postgres-cn/pgdoc-cn/issues/new" target="_blank" title="在Github上报告问题（请注明问题内容及所在章节）">问题报告</a>
+<a style="margin : 0px 0px 0px 10px;" href="https://github.com/postgres-cn/pgdoc-cn/edit/master/postgresql/doc/src/sgml/{0}" target="_blank" title="直接在Github上纠错本页面">纠错本页面</a>
+</div>"""
+
+sgmlDir="../postgresql/doc/src/sgml"
+htmlDir=os.path.join(sgmlDir,"html")
+htmlOutputDir=os.path.join("html_out")
+html2sgmlDict=html2sgml_map.getHtml2sgmlDict(sgmlDir)
+
+if not os.path.exists(htmlOutputDir):
+	os.mkdir(htmlOutputDir)
+	
+# copy stylesheet.css
+open(os.path.join(htmlOutputDir,"stylesheet.css"), "wb").write(open(os.path.join(sgmlDir,"stylesheet.css"), "rb").read())
+
+def process(file):
+	filename=os.path.basename(file)
+	fin=open(file,"r",encoding='GBK')
+	fout=open(os.path.join(htmlOutputDir,filename),"w",encoding='UTF-8')
+	content=fin.read()
+	content=content.replace('CONTENT="text/html; charset=gbk"','CONTENT="text/html; charset=utf-8"')
+	
+	sgmlfile=html2sgmlDict.get(filename.lower())
+	if sgmlfile == None:
+		print(filename+",no sgml")
+		print(content,file=fout)
+	else:
+		print(filename+","+sgmlfile)
+		match=patternInsertPos.match(content)
+		print(content[0:match.end()],file=fout)
+		print(InsertHTML.format(sgmlfile),file=fout)
+		print(content[match.end():],file=fout)
+	fin.close()
+	fout.close()
+	
+for file in os.listdir(htmlDir):
+        if file.endswith(".html"):
+                process(os.path.join(htmlDir,file))
---
+++ tools/html2sgml_map.py
@@ -0,0 +1,31 @@
+#功能:生成html文件到sgml文件的对应关系dict
+
+import os
+import re
+
+patternId=re.compile(r'.*<(book|part|chapter|sect1|refentry|appendix|bibliography|reference|legalnotice|preface) id="([^"]+)"',re.S)
+
+
+def process(file,sgmlprefix,html2sgmlDict):
+	f=open(file,"r",encoding='GBK')
+	filename=os.path.basename(file)
+	for line in f:
+		match=patternId.match(line)
+		if match:
+			html2sgmlDict[match.group(2).lower()+".html"]=sgmlprefix+filename
+
+
+def getHtml2sgmlDict(sgmlDir):
+        html2sgmlDict={}
+        html2sgmlDict["index.html"]="postgres.sgml"
+        for file in os.listdir(sgmlDir):
+                if file.endswith(".sgml"):
+                        process(os.path.join(sgmlDir,file),"",html2sgmlDict)
+                        
+        refDir=os.path.join(sgmlDir,"ref")
+        for file in os.listdir(refDir):
+                if file.endswith(".sgml"):
+                        process(os.path.join(refDir,file),"ref/",html2sgmlDict)
+        return html2sgmlDict
+
+
