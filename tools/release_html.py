600bec381967dd729a2b622dcafd57d96b73199b
Chen Huajun <chjischj@163.com>
2017-11-19 23:02:29 +0800

    release_html.py

---
+++ tools/release_html.py
@@ -0,0 +1,42 @@
+#!/usr/local/python34/bin/python3
+# -*- coding: UTF-8 -*-
+#功能:在html中添加在线纠错链接
+
+import sys
+import os
+import re
+sys.path.append(".") 
+import html2sgml_map
+
+
+sgmlDir="../build/doc/src/sgml"
+htmlDir=os.path.join(sgmlDir,"html")
+htmlOutputDir=os.path.join("html")
+html2sgmlDict=html2sgml_map.getHtml2sgmlDict(sgmlDir)
+
+if not os.path.exists(htmlOutputDir):
+	os.mkdir(htmlOutputDir)
+	
+# copy stylesheet.css
+open(os.path.join(htmlOutputDir,"stylesheet.css"), "wb").write(open(os.path.join(sgmlDir,"stylesheet.css"), "rb").read())
+
+def process(file):
+	filename=os.path.basename(file)
+	fin=open(file,"r",encoding='GBK')
+	fout=open(os.path.join(htmlOutputDir,filename),"w",encoding='UTF-8')
+	content=fin.read()
+	content=content.replace('CONTENT="text/html; charset=gbk"','CONTENT="text/html; charset=utf-8"')
+	
+	sgmlfile=html2sgmlDict.get(filename.lower())
+	if sgmlfile == None:
+		print(filename+",no sgml")
+		print(content,file=fout)
+	else:
+		print(filename+","+sgmlfile)
+		print(content,file=fout)
+	fin.close()
+	fout.close()
+	
+for file in os.listdir(htmlDir):
+        if file.endswith(".html"):
+                process(os.path.join(htmlDir,file))
