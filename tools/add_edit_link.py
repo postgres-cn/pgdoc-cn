fbb2327c9b3f80ea8c03d58a835f74f035fbaa3e
xiongliding <fairplay1986@gmail.com>
2019-10-06 22:38:23 +0800

    给第10版文档加上搜索框

--- tools/add_edit_link.py
+++ tools/add_edit_link.py
@@ -9,10 +9,16 @@ sys.path.append(".")
 import html2sgml_map
 
 patternInsertPos=re.compile(r'.*<body>',re.S)
-InsertHTML="""<div style="text-align:right">
+InsertHTML="""<div class="preheader"><div style="position: relative; overflow: hidden; margin: 10px 0;">
+<form method="GET" action="/v2/doc_search" style="float: right; width: 400px; max-width: 100%;">
+<input type="hidden" name="u" value="/docs/10" />
+<input style="width: 80%; height: 40px; font-size: 1.0em; padding: 0 10px; margin: 0; border: 1px solid #ccc; box-sizing: border-box;" type="text" placeholder="在文档中查找..." name="q"
+/><button style="width: 20%; height: 40px; font-size: 1.0em; padding: 0; margin: 0; border: 1px solid #ccc; box-sizing: border-box; border-left: 0; cursor: pointer;" type="submit" id="#submit">搜索</button>
+</form></div>
+<div style="text-align:right">
 <a style="margin : 0px 0px 0px 10px;" href="https://github.com/postgres-cn/pgdoc-cn/issues/new" target="_blank" title="在Github上报告问题（请注明问题内容及所在章节）">问题报告</a>
 <a style="margin : 0px 0px 0px 10px;" href="https://github.com/postgres-cn/pgdoc-cn/edit/10/postgresql/doc/src/sgml/{0}" target="_blank" title="直接在Github上纠错本页面">纠错本页面</a>
-</div>"""
+</div></div>"""
 
 sgmlDir="../build/doc/src/sgml"
 htmlDir=os.path.join(sgmlDir,"html")
