858192dac48098e40e16396eadb4a9ded28eeef5
ChenHuajun <chjischj@163.com>
2019-10-07 03:18:05 +0800

    添加其他版本链接和全文检索框

--- tools/add_edit_link.py
+++ tools/add_edit_link.py
@@ -1,6 +1,6 @@
 #!/usr/local/python34/bin/python3
 # -*- coding: UTF-8 -*-
-#功能:在html中添加在线纠错链接
+#功能:在html中添加其他版本链接，在线纠错链接和全文检索框
 
 import sys
 import os
@@ -9,10 +9,31 @@ sys.path.append(".")
 import html2sgml_map
 
 patternInsertPos=re.compile(r'.*<body>',re.S)
-InsertHTML="""<div style="text-align:right">
+InsertHTML="""<div class="preheader">
+<div style="float: left">
+<a style="margin : 0px 5px 0px 5px;" href="/docs/9.3/{0}" target="_top">9.3</a>
+<a style="margin : 0px 5px 0px 5px;" href="/docs/9.4/{0}" target="_top">9.4</a> 
+<a style="margin : 0px 5px 0px 5px;" href="/docs/9.5/{0}" target="_top">9.5</a>
+<a style="margin : 0px 5px 0px 5px;" href="/docs/9.6/{0}" target="_top">9.6</a>
+<span style="margin : 0px 5px 0px 5px;">10</span>
+<a style="margin : 0px 5px 0px 5px;" href="/docs/11/{0}" target="_top">11</a>
+</div>
+
+<div style="text-align:right">
 <a style="margin : 0px 0px 0px 10px;" href="https://github.com/postgres-cn/pgdoc-cn/issues/new" target="_blank" title="在Github上报告问题（请注明问题内容及所在章节）">问题报告</a>
-<a style="margin : 0px 0px 0px 10px;" href="https://github.com/postgres-cn/pgdoc-cn/edit/10/postgresql/doc/src/sgml/{0}" target="_blank" title="直接在Github上纠错本页面">纠错本页面</a>
-</div>"""
+<a style="margin : 0px 0px 0px 10px;" href="https://github.com/postgres-cn/pgdoc-cn/edit/master/postgresql/doc/src/sgml/{1}" target="_blank" title="直接在Github上纠错本页面">纠错本页面</a>
+</div>
+
+<div style="position: relative; overflow: hidden; margin: 10px 0;">
+<form method="GET" action="/v2/doc_search" style="float: right; width: 400px; max-width: 100%;">
+<input type="hidden" name="u" value="/docs/10" />
+<input style="width: 80%; height: 40px; font-size: 1.0em; padding: 0 10px; margin: 0; border: 1px solid #ccc; box-sizing: border-box;" type="text" placeholder="在文档中查找..." name="q"
+/><button style="width: 20%; height: 40px; font-size: 1.0em; padding: 0; margin: 0; border: 1px solid #ccc; box-sizing: border-box; border-left: 0; cursor: pointer;" type="submit" id="#submit">搜索</button>
+</form>
+</div>
+
+</div>
+"""
 
 sgmlDir="../build/doc/src/sgml"
 htmlDir=os.path.join(sgmlDir,"html")
@@ -41,7 +62,7 @@ def process(file):
 		print(filename+","+sgmlfile)
 		match=patternInsertPos.match(content)
 		print(content[0:match.end()],file=fout)
-		print(InsertHTML.format(sgmlfile),file=fout)
+		print(InsertHTML.format(filename,sgmlfile),file=fout)
 		print(content[match.end():],file=fout)
 	fin.close()
 	fout.close()
